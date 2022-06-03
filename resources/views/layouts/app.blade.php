6334dccc9727dd4188896906e1658589ba2fc561
Shark-vil <shark_vil@mail.ru>
2022-06-03 04:09:40 +0300

    Make raw pages tamplate

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="author" content="Shark_vil">
+
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+
+	<!-- Scripts -->
+	<script src="{{ asset('js/app.js') }}" defer></script>
+	@yield('scripts')
+
+	<!-- Styles -->
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	@yield('styles')
+
+	<title>Canvas Board</title>
+</head>
+<body>
+	<div class="bg-image bg-blur"></div>
+	<div id="particles" class="bg-particles bg-particles-blur"></div>
+	<div id="app">
+		@yield('content')
+	</div>
+</body>
+</html>
